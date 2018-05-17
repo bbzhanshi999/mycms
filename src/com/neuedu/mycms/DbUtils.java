1a2c7b475c105e757410dae39beebf03e7f014fd
bbzhanshi999 <bbzhanshi999@163.com>
2018-05-17 22:10:05 +0800

    test

---
+++ src/com/neuedu/mycms/DbUtils.java
@@ -0,0 +1,64 @@
+package com.neuedu.mycms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbUtils {
+
+	private static String url = "jdbc:mysql://localhost:3306/news";
+	private static String user = "root";
+	private static String password = "1234";
+	private static String driver = "com.mysql.jdbc.Driver";
+
+	static {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static void free(ResultSet rs, Statement st, Connection conn) {
+		if (null != rs) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (null != st) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (null != conn) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
+
+}
