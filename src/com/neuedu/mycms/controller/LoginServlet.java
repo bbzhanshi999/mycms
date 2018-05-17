b6cbf0daff4c64a57b326d198a69e04daa33eadd
bbzhanshi999 <bbzhanshi999@163.com>
2018-05-17 17:51:34 +0800

    备课

---
+++ src/com/neuedu/mycms/controller/LoginServlet.java
@@ -0,0 +1,28 @@
+package com.neuedu.mycms.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("content", "Hello,World");
+		req.getRequestDispatcher("WEB-INF/index.jsp").forward(req, resp);
+	}
+	
+	
+	
+
+}
